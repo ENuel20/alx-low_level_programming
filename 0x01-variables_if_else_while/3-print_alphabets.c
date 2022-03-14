8fc2f1c98d4cca84aa5caadd19ef7d311b39696e
ENuel20 <emmaonana18@gmail.com>
2022-03-14 01:03:16 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Securit
