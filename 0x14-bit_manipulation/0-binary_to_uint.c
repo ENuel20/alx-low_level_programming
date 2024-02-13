a53d3d654cc56df981665e918accc4559bbc2840
root <emmaonana18@gmail.com>
2024-02-13 11:41:19 -0800

    betty problems solved for bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,3 @@
-
 /**
  * binary_to_uint - function that converts a binary number to an
  * unsigned int
@@ -10,24 +9,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    unsigned int lenght = strlen(b);
-    for ( int i = 0; i < lenght; i++)
-    {
-        if(b[i] == '1')
-        {
-           result = (result << 1) | 1;
-        }
-        else if (b[i] == '0')
-        {
-           result = result << 1;
-        }
-        else{
-             // Invalid character in the binary string
-            printf("Invalid character in binary string: %c\n", b[i]);
-            return 0;
-        }
-    }
-    return result;
+	unsigned int result = 0;
+	unsigned int lenght = strlen(b);
 
+	for (int i = 0; i < lenght; i++)
+	{
+		if (b[i] == '1')
+		{
+			result = (result << 1) | 1;
+		}
+		else if (b[i] == '0')
+		{
+			result = result << 1;
+		}
+		else
+		{
+			printf("Invalid character in binary string: %c\n", b[i]);
+			return (0);
+		}
+	}
+	return (result);
 }
